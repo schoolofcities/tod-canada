9465eee373c5fc873d32811c17340eda9ab478fd
Mieko <37809278+miekoyao@users.noreply.github.com>
2026-03-21 23:54:30 -0400

    update recommendation styling

--- src/lib/Recommendation.svelte
+++ src/lib/Recommendation.svelte
@@ -1,13 +1,10 @@
 <script>
-	// import '../assets/global-styles.css';
-
-	export let style = 1;
 	export let title = "Title";
 	export let count;
 
 </script>
 
-<div class={`style-${style} container`}>
+<div class={`container`}>
 	<div class="box"><h3 class="recommendation">RECOMMENDATION {count}:</h3></div>
 	<h3 class="title">{title}</h3>
 </div>
@@ -16,14 +13,15 @@
 
 	.container {
 		margin-top: 50px;
-		display: flex;
 		position: relative;
 	}
 
 	.box {
-		max-width: 300px;
-  		margin-left: calc(-50% + 45px - 12px);
+		position: absolute;
+		width: 240px;
+  		margin-left: calc(-50% + 45px - 20px);
 		padding: 15px 30px 15px 30px;
+		background-color: #f3f2f3;
 	}
 
 	.recommendation {
@@ -36,58 +34,16 @@
 	}
 
 	.title {
-		padding: 8px 0 15px 15px;
-	}
-
-	
-	.style-1.container, .style-2.container {
-		align-items: start;
+		padding-top: 8px;
+		margin-left: -15px;
+		padding-left: 15px;
+		border-top: 5px solid #f3f2f3;
 	}
 
 	.box h3 {
 		font-size: 22px;
+		text-align: center;
 	}
-
-	.style-1 .box {
-		background-color: #f3f2f3;
-	}
-
-	.style-2 .box, .style-3 .box {
-		width: fit-content;
-		background-color: var(--brandGray80);
-	}
-
-	.style-2 .box h3 {
-		color: var(--brandWhite);
-	}
-
-	.style-1 .title {
-		border-top: 5px solid #f3f2f3;
-	}
-
-	.style-2 .title,.style-3 .title {
-		border-top: 5px solid #f3f2f3;
-	}
-
-	/* .style-2.container,.style-3.container {
-		align-items: stretch;
-	}
-
-	.style-3 .box {
-		display: flex;
-		align-items: center; 
-	}
-
-	.style-4.container {
-		align-items: center;
-	}
-
-	.style-4 .box {
-  		margin-left: calc(-50% + 5px);
-		border-radius: 25px;
-		background-color: rgb(0, 0, 0, 0.1);
-	} */
-
 
 	@media (max-width: 1300px) {
 		.container {
@@ -96,23 +52,20 @@
 		}
 
 		.box {
+			position: relative;
 			margin-top: 50px;
 			width: fit-content;
-			margin-left: 0;
+			margin-left: -20px;
 		}
 
 		.box h3 {
 			font-size: 20px;
 		}
 		
-		.style-1 .title {
+		.title {
 			border-left: 5px solid #f3f2f3;
 			border-top: none;
-		}
-
-		.style-2 .title,.style-3 .title {
-			border-left: 5px solid #f3f2f3;
-			border-top: none;
+			margin-left: -20px;
 		}
 
 	}
